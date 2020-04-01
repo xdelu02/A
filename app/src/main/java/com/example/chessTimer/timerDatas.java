98f53da47d0fea9b4be6ca277273e6cc424ada2e
AleFeri <alessandroferi2002@gmail.com>
2020-04-01 14:06:14 +0200

    Added recover:     -recover path under: RECOVER_WHITE && RECOVER_BLACK     -recover can't be more than 59 seconds     -recover is showed in GUI when the apposit Switch is "Switched" and disappear when is "unSwhitched"
    Added move counter checkbox:
        -move counter is a boolean
        -move counter path: MOVE_COUNTER

--- app/src/main/java/com/example/chessTimer/timerDatas.java
+++ app/src/main/java/com/example/chessTimer/timerDatas.java
@@ -5,24 +5,88 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.util.Log;
+import android.widget.Switch;
 
 public class timerDatas extends AppCompatActivity {
+    //percorsi
     public static final String NAME_WHITE = "com.example.chessTimer.nameWhite";
     public static final String MINUTES_WHITE = "com.example.chessTimer.minutesWhite";
     public static final String SECONDS_WHITE = "com.example.chessTimer.secondsWhite";
+    public static final String RECOVER_WHITE = "com.example.chessTimer.recoverWhite";
     public static final String NAME_BLACK = "com.example.chessTimer.nameBlack";
     public static final String MINUTES_BLACK = "com.example.chessTimer.minutesBlack";
     public static final String SECONDS_BLACK = "com.example.chessTimer.secondsBlack";
+    public static final String RECOVER_BLACK = "com.example.chessTimer.recoverBlack";
+    public static final String MOVE_COUNTER = "com.example.chessTimer.moveCounter";
+
+    private boolean isNeedRecoverWhite = false;
+    private boolean isNeedRecoverBlack = false;
+    private boolean contaMosse = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_datas);
+
+        //funzione che scorre alla attivita' del timer
+        final Button goToTimerBtn = (Button) findViewById(R.id.goToTimerBtn);
+        goToTimerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTimer();
+            }
+        });
+
+        //con lo scorrere dello switch abilita e disabilita la parte del recupero
+        //switch recupero bianco
+        final EditText recoverWhite = (EditText) findViewById(R.id.recoverWhite);
+        final Switch recoverSwitchWhite = (Switch) findViewById(R.id.recoverSwitchWhite);
+        recoverSwitchWhite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    recoverWhite.setVisibility(View.VISIBLE);
+                else
+                    recoverWhite.setVisibility(View.INVISIBLE);
+
+                isNeedRecoverWhite = isChecked;
+            }
+        });
+        //switch recupero nero
+        final EditText recoverBlack = (EditText) findViewById(R.id.recoverBlack);
+        final Switch recoverSwitchBlack = (Switch) findViewById(R.id.recoverSwitchBlack);
+        recoverSwitchBlack.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    recoverBlack.setVisibility(View.VISIBLE);
+                else
+                    recoverBlack.setVisibility(View.INVISIBLE);
+
+                isNeedRecoverBlack = isChecked;
+            }
+        });
+
+        //checkBox del conta mosse
+        final CheckBox moveCounterCheckBox = (CheckBox) findViewById(R.id.moveCounterCheckBox);
+        moveCounterCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    contaMosse = true;
+                else
+                    contaMosse = false;
+            }
+        });
     }
 
-    public int goToTimer (View view) {
+    //funzione che scorre alla attivita' del timer
+    public int goToTimer () {
         Intent intent = new Intent(this, timer.class);
 
         try {
@@ -37,67 +101,89 @@ public class timerDatas extends AppCompatActivity {
         return 0;
     }
 
+    //funzione che setta tutte le variabili da consegnare al timer
     private void setExternalVariables(Intent intent) throws myException {
         //dichiarazione delle variabili
         String nameWhite = "White";
         int minutesWhite = 10;
         int secondsWhite = 0;
+        int recuperoWhite = 0;
         String nameBlack = "Black";
         int minutesBlack = 10;
         int secondsBlack = 0;
+        int recuperoBlack = 0;
 
         //prelevo le EditText
-        EditText nW1EditText = findViewById(R.id.nameWhite);
-        EditText mW1EditText = findViewById(R.id.minuteWhite);
-        EditText sW1EditText = findViewById(R.id.secondWhite);
-        EditText nB1EditText = findViewById(R.id.nameBlack);
-        EditText mB1EditText = findViewById(R.id.minuteBlack);
-        EditText sB1EditText = findViewById(R.id.secondBlack);
+        EditText nWEditText = findViewById(R.id.nameWhite);
+        EditText mWEditText = findViewById(R.id.minuteWhite);
+        EditText sWEditText = findViewById(R.id.secondWhite);
+        EditText rWEditText = findViewById(R.id.recoverWhite);
+        EditText nBEditText = findViewById(R.id.nameBlack);
+        EditText mBEditText = findViewById(R.id.minuteBlack);
+        EditText sBEditText = findViewById(R.id.secondBlack);
+        EditText rBEditText = findViewById(R.id.recoverBlack);
 
         //Se le edit text hanno un valore al loro interno copio il valore nella variabile corrispondente
         //Altrimenti lascio quelli preimpostati in testa
-        if(!nW1EditText.getText().toString().equals(""))
-            nameWhite = nW1EditText.getText().toString();
-        if(!nW1EditText.getText().toString().equals(""))
-            minutesWhite = Integer.parseInt(mW1EditText.getText().toString());
-        if(!sW1EditText.getText().toString().equals(""))
-            secondsWhite = Integer.parseInt(sW1EditText.getText().toString());
-        if(!nB1EditText.getText().toString().equals(""))
-            nameBlack = nB1EditText.getText().toString();
-        if(!mB1EditText.getText().toString().equals(""))
-            minutesBlack = Integer.parseInt(mB1EditText.getText().toString());
-        if(!sB1EditText.getText().toString().equals(""))
-            secondsBlack = Integer.parseInt(sB1EditText.getText().toString());
+        if(!nWEditText.getText().toString().equals("")) //nome White
+            nameWhite = nWEditText.getText().toString();
+        if(!mWEditText.getText().toString().equals("")) //minuti White
+            minutesWhite = Integer.parseInt(mWEditText.getText().toString());
+        if(!sWEditText.getText().toString().equals("")) //secondi White
+            secondsWhite = Integer.parseInt(sWEditText.getText().toString());
+        if(!rWEditText.getText().toString().equals("") && isNeedRecoverWhite) //recupero White
+            recuperoWhite = Integer.parseInt(rWEditText.getText().toString());
+        if(!nBEditText.getText().toString().equals("")) //nome Black
+            nameBlack = nBEditText.getText().toString();
+        if(!mBEditText.getText().toString().equals("")) //minuti Black
+            minutesBlack = Integer.parseInt(mBEditText.getText().toString());
+        if(!sBEditText.getText().toString().equals("")) //secondi Black
+            secondsBlack = Integer.parseInt(sBEditText.getText().toString());
+        if(!rBEditText.getText().toString().equals("") && isNeedRecoverBlack) //recupero Black
+            recuperoBlack = Integer.parseInt(rBEditText.getText().toString());
 
         //controllo che il tempo non superi il limite massimo di 59:59
         if(secondsWhite > 59)
             throw new myException("****** White seconds intervall error ******");
         if(minutesWhite > 59)
             throw new myException("****** White minute intervall error ******");
+        if(recuperoWhite > 59)
+            throw new myException("****** White recover intervall error ******");
         if(secondsBlack > 59)
             throw new myException("****** Black seconds intervall error ******");
         if(minutesBlack > 59)
             throw new myException("****** Black minute intervall error ******");
+        if(recuperoBlack > 59)
+            throw new myException("****** Black recover intervall error ******");
 
         //visualizzo su "RUN" i dati passati
         visualiseLogcat("nameWhite ", "" + nameWhite);
         visualiseLogcat("minutesWhite ", "" + minutesWhite);
         visualiseLogcat("secondsWhite ", "" + secondsWhite);
+        visualiseLogcat("recoverWhite ", "" + recuperoWhite);
         visualiseLogcat("nameBlack ", "" + nameBlack);
         visualiseLogcat("minutesBlack ", "" + minutesBlack);
         visualiseLogcat("secondsBlack ", "" + secondsBlack);
+        visualiseLogcat("recoverBlack ", "" + recuperoBlack);
+        if(contaMosse)
+            visualiseLogcat("contaMosse ", "true");
+        else
+            visualiseLogcat("contaMosse ", "false");
 
         //Aggiungo le variabili come extra
         intent.putExtra(NAME_WHITE, nameWhite);
         intent.putExtra(MINUTES_WHITE, minutesWhite);
         intent.putExtra(SECONDS_WHITE, secondsWhite);
+        intent.putExtra(RECOVER_WHITE, recuperoWhite);
         intent.putExtra(NAME_BLACK, nameBlack);
         intent.putExtra(MINUTES_BLACK, minutesBlack);
         intent.putExtra(SECONDS_BLACK, secondsBlack);
+        intent.putExtra(RECOVER_BLACK, recuperoBlack);
+        intent.putExtra(MOVE_COUNTER, contaMosse);
     }
 
     //metodo provvisorio
-    private void visualiseLogcat(String tag,String str) {
+    private void visualiseLogcat(String tag, String str) {
         Log.d(tag, str);
     }
 }
