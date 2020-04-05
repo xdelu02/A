7bba96848e2366619b8c09097f542cecbf618aff
AleFeri <alessandroferi2002@gmail.com>
2020-04-05 16:55:25 +0200

    Added Settings section.

--- app/src/main/java/com/example/chessTimer/MainActivity.java
+++ app/src/main/java/com/example/chessTimer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TimerDatas
+        Button timerDatasBtn = (Button) findViewById(R.id.timerDatasBtn);
+        timerDatasBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTimerDatas();
+            }
+        });
+
+        //Settings
+        Button settingsBtn = (Button) findViewById(R.id.settingsBtn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSettings();
+            }
+        });
     }
 
-    public void goToTimer (View view){
+    //go to methods
+    //timerDatas
+    private void goToTimerDatas () {
         Intent intent = new Intent(this, timerDatas.class);
+        startActivity(intent);
+    }
+    //Settings
+    private void goToSettings (){
+        Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/chessTimer/Settings.java
@@ -0,0 +1,17 @@
+package com.example.chessTimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //TO DOOOOOOOOOOO
+
+    }
+}
